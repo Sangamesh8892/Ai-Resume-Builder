d65c7c005ec11c53b6500565e1d183851c751340
Sangamesh8892 <sangamesh8892@gmail.com>
2025-11-27 00:40:07 +0530

    Initial commit    feat: add hero section,landing page title and banner

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+    return (
+        <div className="w-full py-2.5 font-medium text-sm text-white text-center bg-gradient-to-r from-[#4F39F6] to-[#bd63d6]">
+            <p><span className="px-3 py-1 rounded-md text-indigo-600 bg-white mr-2 ">New</span>AI features added - ATS check, Job Description Scrore Matcher </p>
+        </div>
+    );
+};
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+
+const Home = () => {
+  return (
+
+    <div>
+      <Banner />
+      <Hero />
+      <Features />
+    </div>
+  )
+}
+
+export default Home
